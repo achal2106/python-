3559d18d0b45524190936d12786f07891ea77360
Achal Narwade <2024bsc120@sggs.ac.in>
2025-06-25 20:08:35 +0530

    added more functions

--- kwarg.py
+++ kwarg.py
@@ -37,3 +37,36 @@ order_summary(
     location="Table 5",
     Time="7.30 PM"
 )
+
+#SPECIAL PARAMETER IN KEYWORD ARGUMENTS(creating function)
+
+#1
+def draw_lines(x1,y1,/,x2,y2,*,color="black",width=1):
+    print(f"Draw from ({x1},{y1}) to ({x2}, {y2}) in {color} with width {width}")
+          
+draw_lines(0,0,10,10,color="red",width=3)
+
+#2
+
+def create_rectangle(x,y,/,height,width,*,fill_color="blue",border_color="black"):
+    print(f"Rectangle at ({x}, {y}) of size ({height} * {width}) with border color {border_color} and fill with {fill_color}")
+
+create_rectangle(0,0,height=80,width=80,fill_color="green")
+
+#as i dont give border color it uses default color as border color
+#and also after'/' i can use these as keywords or positional,before(/) only positional
+
+#3
+
+def draw_circle(x,y,/,radius,*,fill_color="pink",border_color="blue"):
+    print(f"circle at ({x}, {y}) of radius {radius} with {border_color} color and filled with {fil_color}")
+
+draw_circle(0,0,4,fill_color="yellow")
+
+#4
+
+def play_sound(file_path,/,volume,*,loop=False):
+    print(f"Playing {file_path} at volume {volume}, loop: {loop}")
+
+
+play_sound("sound.wav", 0.5, loop=True) 
